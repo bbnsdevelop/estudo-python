3109df1cab9b7fbb36cd45e9031088b03ab96cec
T1508BRN <t1508brn@prestadorcbmp.com.br>
2019-06-10 17:44:38 -0300

    primeiros passos com python

---
+++ curso-python.py
@@ -0,0 +1,112 @@
+a=1
+
+b=2
+
+c=3
+
+
+
+d=a+b+c
+
+
+
+print('soma', d)
+
+
+
+
+
+lista=[1,2,3,4,"bruno", 15,50]
+
+print(lista)
+
+
+
+lista.append("python")
+
+
+
+print(lista)
+
+
+
+lista.append(100)
+
+
+
+print(lista)
+
+
+
+index= lista.index("bruno")
+
+
+
+print("index", index)
+
+
+
+print(lista.count(4))
+
+
+
+lista.append(4)
+
+
+
+print(lista.count(4))
+
+
+lista.remove("python")
+
+print(lista)
+
+
+lista.remove(4)
+
+print(lista)
+
+
+lista.reverse()
+
+print(lista)
+
+
+numeros=[1,2,6,8,5,3,7,4,9]
+
+print(numeros)
+
+
+numeros.sort()
+
+print(numeros)
+
+
+
+# dicionarios
+
+
+telefones={"bruno":999999999,"tacy":888888888, "teste":0000000000}
+
+
+
+print(telefones)
+
+telefones["rita"]=15151515155
+
+print(telefones)
+
+
+del telefones["rita"]
+
+print(telefones)
+
+#endereco =
+
+pessoa={
+ "nome": "bruno Batista",
+ "endereco":{"rua":"Fagundes Varelas"}
+}
+
+
+print(pessoa)
